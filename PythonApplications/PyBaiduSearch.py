1a4849e9d9ed921460a88d27e9ccca2cc8e17a74
ShawnFeng <FML927@live.com>
2019-03-22 23:52:28 +0800

    add random

--- PythonApplications/PyBaiduSearch.py
+++ PythonApplications/PyBaiduSearch.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import time
 import re
+import random
 from pyquery import PyQuery as pq
 
 # 实现自动模拟鼠标点击爬虫
@@ -9,13 +10,27 @@ from pyquery import PyQuery as pq
 # 环境设置参考网页，安装selenium-base，pyquery；下载chrome浏览器和chromedriver
 
 
-def  openurl(url,num):        
-	browser  = webdriver.Chrome()  #打开浏览器        
-	browser.get(url)               #进入相关网站          
+def  openurl(url, click_num):        
+    browser  = webdriver.Chrome()  #打开浏览器        
+    browser.get(url)               #进入相关网站          
+    browser.get(url)               #进入相关网站          
 
-	# 输入 + 点击按钮操作
-	browser.find_element_by_id("kw").send_keys("美女")   
-	browser.find_element_by_id("su").click()  
+    browser.get(url)               #进入相关网站          
+
+    browser.get(url)               #进入相关网站          
+
+    # 输入 + 点击按钮操作
+    browser.find_element_by_id("kw").send_keys("美女")
+    cnt=0
+
+    while(1):	    
+        browser.find_element_by_id("su").click() 
+        cnt+=1
+        print("Click: ",cnt)
+                
+        time.sleep(0.2+ random.randint(0,200)/200.0)  #make 0.2~0.4S delay
+        if(cnt>click_num):
+            break
 	
 	# 因为百度支持输入关键字后  回车搜索  所以这样做是可以的
 	# browser.find_element_by_id("kw").send_keys("美女\r\n")   
@@ -30,5 +45,6 @@ def  openurl(url,num):
 if __name__ == "__main__":
     url='https://www.baidu.com/'
     dict=openurl(url,100)
+
     print(dict)
 
